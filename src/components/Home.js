634c222df0471db90a81f864aebe093137b812fe
Marc-Anderson  Gilles <hornil6748@gmail.com>
2023-02-26 17:01:07 -0500

    arrangement de la page home(fautes ortho)

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ function Home() {
   const questions = [
 		{
 			questionText: 'Qui peut battre Goku?',
-      imageUrl: goku,
+      		imageUrl: goku,
 			answerOptions: [
 				{ answerText: 'Saitama', isCorrect: true },
 				{ answerText: 'Luffy', isCorrect: false },
@@ -24,7 +24,7 @@ function Home() {
 		},
 		{
 			questionText: 'Quand est-ce que One piece est sortie en anime?',
-      imageUrl: onepiece,
+      		imageUrl: onepiece,
 			answerOptions: [
 				{ answerText: '1999', isCorrect: true },
 				{ answerText: '1998', isCorrect: false },
@@ -34,7 +34,7 @@ function Home() {
 		},
 		{
 			questionText: 'Ichigo Kurosaki a combien de races différentes en lui?',
-      imageUrl: ichigo,
+      		imageUrl: ichigo,
 			answerOptions: [
 				{ answerText: '3', isCorrect: false },
 				{ answerText: '5', isCorrect: false },
@@ -44,7 +44,7 @@ function Home() {
 		},
 		{
 			questionText: 'Naruto Uzumaki est devenu lequel Hokage?',
-      imageUrl: naruto,
+      		imageUrl: naruto,
 			answerOptions: [
 				{ answerText: ' Le huitième Hokage', isCorrect: false },
 				{ answerText: 'Le septième Hokage', isCorrect: true },
@@ -53,18 +53,18 @@ function Home() {
 			],
 		},
     {
-			questionText: 'Quel est le but premier but de Natsu Dragnir dans la série Fairy Tail',
-      imageUrl: natsu,
+			questionText: 'Quel est le premier but de Natsu Dragnir dans la série Fairy Tail',
+      		imageUrl: natsu,
 			answerOptions: [
-				{ answerText: 'Retrouver son père', isCorrect: false },
+				{ answerText: 'Retrouver son père', isCorrect: true },
 				{ answerText: 'Tuer un dragon', isCorrect: false },
 				{ answerText: 'Rejoindre la guilde de Fairy Tail ', isCorrect: false },
-				{ answerText: 'Se faire des amis', isCorrect: true },
+				{ answerText: 'Se faire des amis', isCorrect: false },
 			],
 		},
     {
-			questionText: 'Quel est le but premier but de Natsu Dragnir dans la série Demon Slayer',
-      imageUrl: tanjiro,
+			questionText: 'Quel est le but premier but de Tanjiro Kamado dans la série Demon Slayer',
+      		imageUrl: tanjiro,
 			answerOptions: [
 				{ answerText: 'Guérir sa soeur', isCorrect: true },
 				{ answerText: 'Tuer le démon qui a tuer sa famille', isCorrect: false },
@@ -74,7 +74,7 @@ function Home() {
 		},
     {
 			questionText: 'Quels animés font partis "du Big 3" ',
-      imageUrl: big3,
+      		imageUrl: big3,
 			answerOptions: [
 				{ answerText: 'One piece, Naruto, Dragon Ball', isCorrect: false },
 				{ answerText: 'My Hero Academia, Black Clover, Demon Slayer', isCorrect: false },
@@ -83,71 +83,75 @@ function Home() {
 			],
 		},
 	];
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-	const [score, setScore] = useState(0);
+
+
+   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
   const [wrongAnswers, setWrongAnswers] = useState([]);
 
+  const handleAnswerOptionClick = (isCorrect, answerText) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    } else {
+      setWrongAnswers([...wrongAnswers, { question: questions[currentQuestion].questionText, answer: answerText }]);
+    }
 
-	const handleAnswerOptionClick = (isCorrect, answerText) => {
-		if (isCorrect) {
-			setScore(score + 1);
-		} else {
-			setWrongAnswers([...wrongAnswers, { question: questions[currentQuestion].questionText, answer: answerText }]);
-		}
-
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questions.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			setShowScore(true);
-		}
-	};
-  
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
 
   return (
     <div className='quiz-container'>
-    {showScore ? (
-      <div className='quiz'>
-        <p>Tu as réussi {score} sur {questions.length} questions</p>
-  <p>questions échouées :</p>
-  <ul className='listReponsesFauses'>
-    {wrongAnswers.map((answer, index) => (
-      <li key={index} className='wrong-answer'>{answer.question} - {answer.answer}</li>
-    ))}
-  </ul>
-  <button className='recommencer-button' onClick={() => {
-    setShowScore(false);
-    setScore(0);
-    setCurrentQuestion(0);
-    setWrongAnswers([]);
-  }}>
-    Recommencer
-  </button>
-</div>
-    ) : (
-      <>
-      <div className='quizContainerC'>
-
-        <div className='quiz-section'>
-          <div className='quiz-count'>
-            <span>Question {currentQuestion + 1}</span>/{questions.length}
+      {showScore ? (
+        <div className='quiz'>
+          <p>Tu as réussi {score} sur {questions.length} questions</p>
+          <p>questions échouées :</p>
+          <ul className='listReponsesFauses'>
+            {wrongAnswers.map((answer, index) => (
+              <li key={index} className='wrong-answer'>{answer.question} - {answer.answer}</li>
+            ))}
+          </ul>
+          <button className='recommencer-button' onClick={() => {
+            setShowScore(false);
+            setScore(0);
+            setCurrentQuestion(0);
+            setWrongAnswers([]);
+          }}>
+            Recommencer
+          </button>
+        </div>
+      ) : (
+        <>
+          <div className='quizContainerC'>
+            <div className='quiz-section'>
+              <div className='quiz-count'>
+                <span>Question {currentQuestion + 1}</span>/{questions.length}
+              </div>
+              <div className='quiz-image'>
+                <img className='imageQuestion' src={questions[currentQuestion].imageUrl} alt='Question' />
+              </div>
+              <div className='quiz-text'>{questions[currentQuestion].questionText}</div>
+            </div>
+            <div className='answer-section'>
+              {questions[currentQuestion].answerOptions.map((answerOption, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleAnswerOptionClick(answerOption.isCorrect, answerOption.answerText)}
+                  className={answerOption.isCorrect ? 'correct-answer' : 'wrong-answer'}
+                >
+                  {answerOption.answerText}
+                </button>
+              ))}
+            </div>
           </div>
-          <div className='quiz-image'>
-            <img className='imageQuestion' src={questions[currentQuestion].imageUrl} alt='Question' />
-          </div>
-          <div className='quiz-text'>{questions[currentQuestion].questionText}</div>
-        </div>
-        <div className='answer-section'>
-          {questions[currentQuestion].answerOptions.map((answerOption) => (
-            <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-          ))}
-        </div>
-
-        </div>
-      </>
-    )}
-  </div>
+        </>
+      )}
+    </div>
   );
 }
 
